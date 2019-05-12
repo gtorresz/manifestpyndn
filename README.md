3f013744c0e73d1b7f5dfc829e1732cab844f243
Alexander Afanasyev <aa@cs.fiu.edu>
2019-05-12 18:10:52 -0400

    Finalize the experiment

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # manifestpyndn
 
+To setup the experiment:
 
+0. Install dependencies
+
+        pip3 install -r requirements.txt
+
+1. Setup NDN-ABS authority and attribute set for the experiment
+
+        ndnabs setup /icn2019/test/authority
+
+        ndnabs gen-secret attribute1 attribute2 | ndnabs install-secret
+
+3. Generate appropriate sized file
+
+        head -c 10M </dev/urandom > myfile
+
+4. Run the experiment and record the result
+
+        ./abs_experiment.py myfile
--- abs_experiment.py
+++ abs_experiment.py
@@ -8,6 +8,8 @@ import socket
 import sys
 import tempfile
 import os
+import timeit
+import numpy
 
 from pyndn import Blob, Data, Face, Name, DigestSha256Signature
 import ndnabs
@@ -20,54 +22,45 @@ SHA256_DIGEST_SIZE = 32
 class Experiment:
 
     _maxSegmentPayloadLength = 8192
-    
-    def __init__(self):
-        parser = argparse.ArgumentParser(description = 'NDN-ABS experiment')
-        parser.add_argument('file', help='''File to encode in chunks and sign''')
-        parser.add_argument('groupSize', type=int, help='''Group size (the number of digests per manifest file)''')
-        parser.add_argument('nAttributes', type=int, help='''Number of ABS attributes in the signature. Note that each experiment creates new authority, creates and uses the new set of attributes using `attributeN` (where N is 1, 2, 3, ... ) pattern''')
-        parser.add_argument('--keep-data', action='store_true', help='''Keep all generated chunks in memory''')
 
-        args = parser.parse_args()
-        if args.nAttributes < 1 or args.nAttributes > 10:
-            parser.print_help()
-            raise Error("The number of ABS attributes must be between 1 and 10")
+    def __init__(self, filename, groupSize, nAttributes, absPath, keepData = False):
 
-        self._setupAbs()
-        self._generateAttributes(args.nAttributes)
-        self._readDataAndCreateManifests(args.file, args.groupSize, args.keep_data)
+        self.attributes = [b'attribute%d' % i for i in range(1, nAttributes + 1)]
+        self._setupAbs(absPath)
+        self._readDataAndCreateManifests(filename, groupSize, keepData)
 
-    def _setupAbs(self):
-        
-        fd, tmpDbPath = tempfile.mkstemp()
-        os.close(fd)
-        os.remove(tmpDbPath)
-        print ("Creating NDN-ABS authority and signer in %s" % tmpDbPath)
-        self.db = ndnabs.PickleDb(tmpDbPath)
-        
-        self.authority = ndnabs.AttributeAuthority(self.db)
+    def _setupAbs(self, absPath):
+
+        # sys.stderr.write ("Using NDN-ABS authority, signer, and verifier database from %s\n" % absPath)
+        self.db = ndnabs.PickleDb(absPath)
+
         self.signer = ndnabs.Signer(self.db)
         self.verifier = ndnabs.Verifier(self.db)
 
-        self.authority.setup(Name("/icn2019/test/authority"))
-        # databse for authority, verifier, and signer are shared, so no need to get and install public parameters
+        try:
+            info = self.signer.get_public_params_info()
+            if info.getName().getPrefix(-2).toUri() != "/icn2019/test/authority":
+                raise RuntimeError('NDN-ABS authority exists, but not setup for experiment. Use `ndnabs setup -f /icn2019/test/authority` to force-setup the authority')
+        except:
+            raise RuntimeError("Public parameters are not properly installed for the signer/verifier")
 
-    def _generateAttributes(self, nAttrs):
-        self.attributes = [b'attribute%d' % i for i in range(1, nAttrs + 1)]
-        secret = self.authority.gen_attr_keys(self.attributes)
-        self.signer.install_secret(secret)
+        for attr in self.attributes:
+            if not attr in self.signer.get_attributes():
+                raise RuntimeError("%s attribute missing. Generate attributes for the experiment using `ndnabs gen-secret %s | ndnabs install-secret`" % (str(attr, 'utf-8'), ' '.join([str(i, 'utf-8') for i in self.attributes])))
 
     def _createManifest(self, name, manifestBuffer, nManifests):
         manifest = Data(name)
         manifest.setContent(manifestBuffer[0:nManifests * SHA256_DIGEST_SIZE])
         return manifest
-        
+
     def _readDataAndCreateManifests(self, filename, groupSize, keepData):
         if groupSize < 1:
             raise RuntimeError("Group size cannot be less than 1")
-        
-        self.allChunks = []            # for holding the generated data packets, including unsigned manifests
-        self.allUnsignedManifests = [] # for storing only unsigned manifest packets
+
+        self.allChunks = []    # for holding the generated data packets, including unsigned manifests
+        self.allManifests = [] # for storing first unsigned manifest packets, which are then signed in-place
+        self.rawDataCount = 0
+        self.ndnChunkCount = 0
 
         seqNo = 0   # sequence number of data packets
         chunkNo = 0 # number of the chunk in the group
@@ -79,11 +72,12 @@ class Experiment:
                 return bytearray(groupSize * SHA256_DIGEST_SIZE)
 
             digests = allocateBufferForDigests()
-            
+
             while f.readable():
                 chunkPayload = f.read(self._maxSegmentPayloadLength)
                 if len(chunkPayload) == 0:
                     break
+                self.rawDataCount = self.rawDataCount + len(chunkPayload)
 
                 chunk = Data(Name("/icn2019/test/data").appendSequenceNumber(seqNo))
                 seqNo = seqNo + 1
@@ -96,34 +90,81 @@ class Experiment:
                 if keepData:
                     self.allChunks.append(chunk)
 
+                # only data chunks; manifest sizes counted separatedly, as they are signed
+                self.ndnChunkCount = self.ndnChunkCount + chunk.wireEncode().size()
+
                 # For storing data packet to a file
                 # with open(writepath + "-1.txt", "wb") as dataf
                 #     dataf.write(dpacket_bytes)
-                    
+
                 implicitDigest = chunk.getFullName()[-1].getValue()
 
                 offset = chunkNo * SHA256_DIGEST_SIZE
                 digests[offset:offset + SHA256_DIGEST_SIZE] = implicitDigest.toBytes()[:]
 
-                if (seqNo % groupSize) == groupSize - 1:
+                chunkNo = chunkNo + 1
+
+                if chunkNo == groupSize:
                     manifest = self._createManifest(Name("/icn2019/test/data").appendSequenceNumber(seqNo), digests, groupSize) # full group
                     seqNo = seqNo + 1
                     self.allChunks.append(manifest)
-                    self.allUnsignedManifests.append(manifest)
+                    self.allManifests.append(manifest)
                     chunkNo = 0
                     digests = allocateBufferForDigests()
-
-                chunkNo = chunkNo + 1
 
             if chunkNo != 0:
                 manifest = self._createManifest(Name("/icn2019/test/data").appendSequenceNumber(seqNo), digests, groupSize) # partial group
                 self.allChunks.append(manifest)
-                self.allUnsignedManifests.append(manifest)
+                self.allManifests.append(manifest)
+
+            self.nDataChunks = seqNo - len(self.allManifests) # number of data packets, excluding the manifests
+
+    def signManifests(self):
+        self.manifestCount = 0
+        self.signatureCounts = []
+        for manifest in self.allManifests:
+            self.signer.sign(manifest, self.attributes)
+            self.manifestCount = self.manifestCount + manifest.wireEncode().size()
+            self.signatureCounts.append(manifest.getSignature().getSignature().size())
+
+    def verifyManifests(self):
+        for manifest in self.allManifests:
+            self.signer.verify(manifest)
+
+def main():
+    parser = argparse.ArgumentParser(description = 'NDN-ABS experiment')
+    parser.add_argument('file', help='''File to encode in chunks and sign''')
+    parser.add_argument('--minGroupSize', type=int, default=1, help='''Min manifestt group size (default: 1)''')
+    parser.add_argument('--maxGroupSize', type=int, default=10, help='''Max manifest group size (default: 10)''')
+    parser.add_argument('--minAttributes', type=int, default=1, help='''Min number of attributes (default: 1)''')
+    parser.add_argument('--maxAttributes', type=int, default=10, help='''Max number of attributes (default: 10)''')
+    parser.add_argument('--keep-data', action='store_true', help='''Keep all generated chunks in memory''')
+    parser.add_argument('--abs-path', default=os.path.expanduser('~/.ndn/ndnabs.db'), help='''Set path for security database (default: ~/.ndn/ndnabs.db)''')
+
+    args = parser.parse_args()
+
+    print ("GroupSize,NAttributes,SignTime,VerifyTime,RawDataSize,NdnDataSize,NManifests,NData,MinAbsSignatureSize,MaxAbsSignatureSize,AvgAbsSignatureSize,StdAbsSignatureSize")
+
+    for groupSize in range(args.minGroupSize, args.maxGroupSize + 1):
+        for nAttributes in range(args.minAttributes, args.maxAttributes + 1):
+            experiment = Experiment(args.file, groupSize, nAttributes, args.abs_path, args.keep_data)
+            # for i in experiment.allChunks:
+            #     print ("name:", i.getName(), "size:", len(i.wireEncode().toBytes()), "contentSize:", i.getContent().size())
+            # continue
+
+            signTime = timeit.timeit(stmt='experiment.signManifests()', number=10, globals={**globals(), **locals()}) # sign all manifests 10 times
+            verifyTime = 0 #timeit.timeit(stmt='experiment.verifyManifests()', number=10, globals={**globals(), **locals()}) # sign all manifests 10 times
+
+            # this requires Python 3.6
+            print (f"{groupSize},{nAttributes}" +
+                   f",{signTime / experiment.manifestCount},{verifyTime / experiment.manifestCount}" +
+                   f",{experiment.rawDataCount},{experiment.ndnChunkCount + experiment.manifestCount}" +
+                   f",{len(experiment.allManifests)},{experiment.nDataChunks}"
+                   f",{numpy.min(experiment.signatureCounts)},{numpy.max(experiment.signatureCounts)}" +
+                   f",{numpy.mean(experiment.signatureCounts)},{numpy.std(experiment.signatureCounts)}")
 
 if __name__ == "__main__":
     try:
-        experiment = Experiment()
-        # for i in experiment.allChunks:
-        #     print ("name:", i.getName(), "size:", len(i.wireEncode().toBytes()))
-    except Error as e:
-        print (e)
+        main()
+    except RuntimeError as e:
+        sys.stderr.write("ERROR: %s\n" % str(e))
